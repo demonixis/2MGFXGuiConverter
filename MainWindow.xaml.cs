ad85e666e9c5c56264b70d1df8eda721d18da912
Yann <comte.yannick@gmail.com>
2012-12-30 19:02:35 +0100

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Diagnostics;
+
+namespace _2MGFX_Converter
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string _twoMGFXExecutable;
+        private string _twoMGFXPath;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+
+            _twoMGFXExecutable = "2MGFX.exe";
+
+            // By Default the 2MGFX executable is in the same directory
+            _twoMGFXPath = String.Empty;
+
+            // Or use a proper directory
+            if (Directory.Exists(System.IO.Path.Combine(Directory.GetCurrentDirectory(), "2MGFX")))
+                _twoMGFXPath = "2MGFX";
+
+            // Test if file exist
+            if (!File.Exists(System.IO.Path.Combine(_twoMGFXPath, _twoMGFXExecutable)))
+            {
+                MessageBox.Show("2MGFX.exe is missing", "2MGFX is missing", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+                Application.Current.Shutdown();
+            }
+        }
+
+        /// <summary>
+        /// Launch the conversion when the user drop a file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnStackPanel_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data is System.Windows.DataObject && ((System.Windows.DataObject)e.Data).ContainsFileDropList())
+            {
+                bool hasValidFile = false;
+
+                foreach (string filePath in ((System.Windows.DataObject)e.Data).GetFileDropList())
+                {
+                    if (filePath.Contains(".fx"))
+                    {
+                        hasValidFile = true;
+
+                        Process process = new Process();
+                        process.StartInfo.Arguments = GetCommandArgs(filePath);
+                        process.StartInfo.WorkingDirectory = _twoMGFXPath;
+                        process.StartInfo.FileName = _twoMGFXExecutable;
+                        process.Start();
+                    }
+                    else
+                    {
+                        MessageBox.Show("This isn't a valid effect file", "Invalid file", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+                    }
+                }
+
+                if (hasValidFile)
+                {
+                    MessageBox.Show("Conversion done", "Information", MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.OK);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get command line arguments for 2MGFX
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        private string GetCommandArgs(string filePath)
+        {
+            string profile = shaderProfile.SelectedIndex > 0 ? "dx11" : "ogl";
+
+            // Shader profile
+            string extra = profile == "DX" ? " /DX11" : String.Empty;
+            
+            // Define the good extension
+            string mgsExt = "mgfxo";
+            if (usePrefix.IsChecked.Value == true)
+                mgsExt = String.Format("{0}.mgfxo", profile);
+             
+            // Define the new path
+            string newFilePath = filePath.Replace("fx", mgsExt);
+            
+            return filePath + " " + newFilePath + extra;
+        }
+
+        private void usePrefix_Checked_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 2MGFXGuiConverter
 =================
 
-A simple drag &amp; drop shader converter for 2MGFX
+A simple drag &amp; drop shader converter for 2MGFX. Convert all your effect without the use of command line, just with a simple drag & drop. You can select the profile you want (OpenGL or DirectX11).
+
+## How to use it ?
+
+Just compile it and place 2MGFX.exe (with all its dlls) in a folder named "2MGFX" or in the same folder. 
+
+## About MonoGame project
+
+MonoGame is an OpenSource, OpenGL implementation of Microsoft's XNA 4 APIs, find [more information here](https://github.com/mono/MonoGame "MonoGame on github")
